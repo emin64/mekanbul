11945a76f1b91141a7517b5ec483b1185b0ab4f7
emin64 <mhmtmncngz@gmail.com>
2022-11-11 22:01:12 +0300

    4.gönderim

---
+++ app_server/models/db.js
@@ -0,0 +1,28 @@
+var mongoose=require("mongoose");
+// var dbURI="mongodb+srv://mekan32:1234@mekanbul.ehouueb.mongodb.net/?retryWrites=true&w=majority";
+var dbURI="mongodb://localhost/mekanbul";
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+        mongoose.connection.close(function(){
+                console.log(msg);
+                callback();
+
+        });
+        
+}
+process.on("SIGINI",function(){
+        kapat("Uygulama kapatıldı",function(){
+                process.exit(0);
+        });
+});
+mongoose.connection.on("connected",function(){
+        console.log(dbURI+" adresindeki veritabanına bağlanıldı.");
+});
+mongoose.connection.on("disconnected",function(){
+        console.log("Bağlantı koptu.");
+});
+mongoose.connection.on("error",function(){
+        console.log("Bağlantı hatası.");
+});
+
+require("./mekansema");
